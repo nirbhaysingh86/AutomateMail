22ef30ec89667f21163b24d92cddd84ac9178663
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2021-10-23 01:59:43 -0500

    Added

--- AutomateMail/Controllers/MailController.cs
+++ AutomateMail/Controllers/MailController.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AutomateMail.Controllers
@@ -13,10 +16,7 @@ namespace AutomateMail.Controllers
     [Route("[controller]")]
     public class MailController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+
 
         private readonly ILogger<MailController> _logger;
 
@@ -26,20 +26,7 @@ namespace AutomateMail.Controllers
         }
 
         [HttpGet]
-        public async Task<Hashtable>  GetAttachmentFile()
-        {
-            MailReader mailReader = new MailReader("imap.gmail.com", 993, true, "nirbhay.us.2020@gmail.com", "nir@US2020");
-            var allUnreadEmailAttachment = mailReader.GetUnreadMails(); 
-            foreach (var email in allUnreadEmailAttachment)
-            {
-                Console.WriteLine(email);
-            }
-            var attachmentList=Task.Run(()=> allUnreadEmailAttachment).ConfigureAwait(false);
-            return await attachmentList;
-        }
-        [HttpPost]
-        [Route("mail/saveattachmentdata")]
-        public async Task<Hashtable> SaveAttachmentData([FromBody] Attachment data)
+        public async Task<Hashtable> GetAttachmentFile()
         {
             MailReader mailReader = new MailReader("imap.gmail.com", 993, true, "nirbhay.us.2020@gmail.com", "nir@US2020");
             var allUnreadEmailAttachment = mailReader.GetUnreadMails();
@@ -50,5 +37,18 @@ namespace AutomateMail.Controllers
             var attachmentList = Task.Run(() => allUnreadEmailAttachment).ConfigureAwait(false);
             return await attachmentList;
         }
+        [HttpPost]
+        [Route("saveattachmentdata")]
+        public async Task<string> SaveAttachmentData()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                string str = await reader.ReadToEndAsync();
+            }
+            DeleteAttachment.Delete();
+            var attachmentList = Task.Run(() => "OK").ConfigureAwait(false);
+            return await attachmentList;
+        }
+
     }
 }
---
+++ AutomateMail/Services/DeleteAttachment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text;
+using System.Linq;
+using ExcelDataReader;
+using System.Collections;
+
+namespace AutomateMail
+{
+
+    public static class DeleteAttachment
+    {
+
+        public static void Delete()
+        {
+            try
+            {
+                var sourceDir = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory).Split("\\bin")[0];
+                string[] xlsxList = Directory.GetFiles(sourceDir, "*.xlsx");
+                // Delete source files that were copied.
+                foreach (string f in xlsxList)
+                {
+                    File.Delete(f);
+                }
+
+            }
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+        }
+    }
+}
